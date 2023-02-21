77039967ba2224ff12ec736eea581417bbbaabf3
Aammya Sapra <79061216+doraemon127@users.noreply.github.com>
2023-02-20 20:32:17 -0800

    Setup File Structure

--- hdc-lib/encoding/id-level/id-lvl/id-lvl.ino
+++ hdc-lib/encoding/id-level/id-lvl/id-lvl.ino
@@ -1,3 +1,46 @@
+/*
+ID-Level Encoding
+*/
+
+#define DIMENSIONS 10000
+#define NUM_LEVELS 100
+
+
+struct ID_Level_Encoder {
+
+  float id[];
+  float value[];
+
+  ID_Level_Encoder() {
+    id = 
+  }
+
+
+
+
+
+}
+
+
+
+void bind() {
+
+  
+
+}
+
+
+void hard_quantize() {
+
+
+}
+
+
+void id_level_encoder(int num_classes, int size) {
+
+}
+
+
 void setup() {
   // put your setup code here, to run once:
 
