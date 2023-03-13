e519c760e00247bd1c0174f8e1e0d19168b2a104
Aammya Sapra <79061216+doraemon127@users.noreply.github.com>
2023-03-12 18:40:36 -0700

    Restructured code, progress on level_hv()

--- hdc-lib/encoding/id-level/id-lvl/id-lvl.ino
+++ hdc-lib/encoding/id-level/id-lvl/id-lvl.ino
@@ -2,32 +2,27 @@
 ID-Level Encoding
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "id-lvl.h"
+#include "isolet.h"
 
 
-#define DIMENSION 10000   // Size of hypervector
-#define NUM_LEVELS 100
+ID_Level_Encoder::ID_Level_Encoder(int n) {
+      num_vectors = n;
+      id = random_hv();
+      value = level_hv();
+}
 
 
-class ID_Level_Encoder {
 
-  public:
-    float id[];
-    float value[];
-    int num_vectors;
-    float sample_hv[];
-
-    /* 
-     * Return array of random binary hypervectors (1 and -1)
-     * Each row in array corresponds to different random hypervector
-     * e.g. row 0 corresponds to hv0, row 1 to hv1, etc. 
-    */
-    float[][] random_hv() {
+/* 
+  * Return array of random binary hypervectors (1 and -1)
+  * Each row in array corresponds to different random hypervector
+  * e.g. row 0 corresponds to hv0, row 1 to hv1, etc. 
+*/
+double[][] ID_Level_Encoder::random_hv() {
 
       // 2-D array to store all num_vectors random binary hypervectors
-      float random_hv_set[num_vectors][DIMENSION];
+      double random_hv_set[num_vectors][DIMENSION];
 
       // Generate random hypervectors
       // https://www.includehelp.com/c-programs/generate-random-numbers-within-a-range.aspx
@@ -36,85 +31,115 @@ class ID_Level_Encoder {
       srand(time(0));
       for (int hv = 0; hv < num_vectors; hv++) {
         for (int element = 0; element < DIMENSION; element++) {
-          random_hv_set[hv][element] = (float) (rand() % (upper - lower + 1)) + lower;
+          random_hv_set[hv][element] = (double) (rand() % (upper - lower + 1)) + lower;
           printf("%f ", random_hv_set[hv][element]);
         }
         printf("\n");
       }
 
       return random_hv_set;
-    }
-
-
-    /*
-     * 
-     */
-    float[] level_hv() {
-
-    }
-  
-    float[][] bind(float [] weight, float [] value) {
-      float hv[][];
-      for (int i = 0; i < weight.size(); i++){
-        for (int j = 0; i < weight[0].size(); i++){
-          if (weight[i][j]*value[i][j] > 0)
-            hv[i][j] = 1;
-          else
-            hv[i][j] = 0;
-        }
-      }
-      return hv;
-    }
-
-    float[] multiset(float [] sample) {
-      float hv[];
-      for (int i = 0; i < sample[0].size(); i++){
-        float total;
-        for (int j = 0; j < sample[0].size(); i++)
-          total += sample[i][j];
-          hv[i] = total;
-        }
-        return hv;
-    }
-
-    float[] hard_quantize(float [] sample){
-      float hv[];
-      for (int i = 0; i < sample.size(); i++){
-        if (sample[i] > 0)
-          hv[i] = 1;
-        else
-          hv[i] = 0;
-      }
-      return hv;
-    }
-
-
-    /* Constructor */
-    ID_Level_Encoder(int n) {
-      num_vectors = n;
-      id = random_hv();
-      value = level_hv();
-      
-    }
-  
-    ID_Level_Forward(float[] id, float [] x){
-      sample_hv = multiset(bind(id,x));
-      return hard_queantize(sample_hv);
-
-    }
-  private:
-
 }
 
 
 
 
-// void id_level_encoder(int num_classes, int size) {}
+double[][] ID_Level_Encoder::level_hv() {
+  int levels_per_span = (1 - RANDOMNESS) * (num_vectors - 1) + RANDOMNESS * 1; // RANDOMNESS defined as 0
+  levels_per_span = max(levels_per_span, 1);
+  int span = (num_vectors - 1) / levels_per_span;
+
+  double[ceil(span+1)][DIMENSION] span_hv; // 2 x DIMENSION
+
+  // TODO: generate 2 orthogal hv FOR SPAN_HV
+
+  double[ceil(span)][DIMENSION] threshold_v; // 1 x DIMENSION
+
+  // TODO random generate threshold
+  // https://stackoverflow.com/questions/288739/generate-random-numbers-uniformly-over-an-entire-range
+  // https://cplusplus.com/reference/random/uniform_real_distribution/
+  const double range_from = 0.0;
+  const double range_to = 1.0;
+  std::random_device                  rand_dev;
+  std::mt19937                        generator(rand_dev());
+  std::uniform_int_distribution<double>  distr(range_from, range_to);
+
+
+
+  double[num_vectors][DIMENSION] hv; // empty hypervectors
+
+  for (int i = 0; i < num_vectors; i++) {
+    int span_idx = (int) floor(i / levels_per_span);
+
+    // NOTE: special case check ignored for now
+
+    int level_within_span = i % levels_per_span
+
+    double t = 1 - (level_within_span / levels_per_span);
+
+    double[] span_start_hv = span_hv[span_idx];
+    double[] span_end_hv = span_hv[span_idx + 1];
+    hv[i] = (threshold_v[span_idx] < t) ? span_start_hv : span_end_hv;
+  }
+
+
+  return hv;
+}
+
+
+
+double[][] ID_Level_Encoder::bind(double [] weight, double [] value) {
+  double hv[][];
+  for (int i = 0; i < weight.size(); i++){
+    for (int j = 0; i < weight[0].size(); i++){
+      if (weight[i][j]*value[i][j] > 0)
+        hv[i][j] = 1;
+      else
+        hv[i][j] = 0;
+    }
+  }
+  return hv;
+}
+
+
+
+double[] ID_Level_Encoder::multiset(double [] sample) {
+  double hv[];
+  for (int i = 0; i < sample[0].size(); i++){
+    double total;
+    for (int j = 0; j < sample[0].size(); i++)
+      total += sample[i][j];
+      hv[i] = total;
+    }
+    return hv;
+}
+
+
+
+double[] ID_Level_Encoder::hard_quantize(double [] sample){
+  double hv[];
+  for (int i = 0; i < sample.size(); i++){
+    if (sample[i] > 0)
+      hv[i] = 1;
+    else
+      hv[i] = 0;
+  }
+  return hv;
+}
+
+
+
+
+double[] ID_Level_Encoder::ID_Level_Forward(double[] id, double [] x) {
+  
+}
+
+
 
 
 void setup() {
   // put your setup code here, to run once:
-
+  int num_vec = isolet.size();
+  ID_Level_Encoder encoder = new ID_Level_Encoder(num_vec);
 }
 
 void loop() {
---
+++ hdc-lib/encoding/id-level/id-lvl/id_lvl.h
@@ -0,0 +1,47 @@
+#ifndef id_lvl_h
+#define id_lvl_h
+
+#include "Arduino.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+#define DIMENSION 10000   // Size of hypervector
+#define NUM_LEVELS 100
+#define RANDOMNESS 0
+
+
+// https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide
+
+class ID_Level_Encoder {
+
+  public:
+
+    ID_Level_Encoder(int);
+    double[][] random_hv();
+    double[][] level_hv();
+    double[][] bind(double [] weight, double [] value);
+    double[] multiset(double [] sample);
+    double[] hard_quantize(double [] sample);
+    double[] ID_Level_Forward(double[] id, double [] x);
+
+  private:
+    double id[];
+    double value[];
+    int num_vectors;
+    double sample_hv[];
+
+};
+
+
+
+
+#endif
